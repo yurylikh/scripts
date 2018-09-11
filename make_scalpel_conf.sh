7a981c9bdc02014a56efcb041d272dc22c917e59
Yury Likh <yury.likh@gmail.com>
2018-09-11 22:44:27 +0200

    Prepares line for scalpel config.

--- make_scalpel_conf.sh
+++ make_scalpel_conf.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-function get_extension {
+function print_extension {
   echo $(stat -c %n "$1" | rev | cut -d. -f1 | rev)
 }
 
+function compose_line_for {
+  file="$1"
+  header=$(hexdump -n 8 -e '"\\\x" 1/1 "%02x"' -v "$file")
+  footer=$(hexdump -n 8 -e '"\\\x" 1/1 "%02x"' -v "$file"\
+                        -s $(($(stat -c %s "$file")-8)) )
+#  echo "$file"": has size "$(stat -c %s "$file")" bytes"
+  echo $(print_extension "$file")" yes ""$header"":""$footer"
+}
 
-for file in ./*
-do
-  if [ -f "$file" ] ; then
-#    echo "$file"": has size "$(stat -c %s "$file")" bytes"
-    header=$(hexdump -n 8 -e '"\\\x" 1/1 "%02x"' -v "$file")
-    footer=$(hexdump -n 8 -e '"\\\x" 1/1 "%02x"' -v "$file"\
-                      -s $(($(stat -c %s "$file")-8)) )
-    echo $(get_extension "$file")" yes ""$header"":""$footer"
-fi
+
+for file in ./* ; do
+  if [ -f "$file" ] && [ -s "$file" ] ; then
+    echo $(compose_line_for "$file")
+  fi
 done
